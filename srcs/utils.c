f9e4caf6690bf70db9a2190f2a45c5e9e3955c37
Ömer Faruk Kuzu <151129176+omrfrkzu@users.noreply.github.com>
2025-02-20 18:37:24 +0300

    Add files via upload

---
+++ srcs/utils.c
@@ -0,0 +1,49 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "utils.h"
+
+void	error_message(char *text, int signal)
+{
+	if (text)
+		write(2, text, ft_strlen(text) + 1);
+	exit(signal);
+}
+
+void	destroy_all(t_engine *engine, char *str, int count, int signal)
+{
+	while (--count >= 0)
+		pthread_mutex_destroy(&engine->forks[count]);
+	pthread_mutex_destroy(&engine->write_lock);
+	pthread_mutex_destroy(&engine->meal_lock);
+	error_message(str, signal);
+}
+
+void	print_action(t_philo *philo, char *action)
+{
+	size_t	time;
+
+	pthread_mutex_lock(philo->mutexes.write_lock);
+	time = get_current_time() - philo->times.born_time;
+	printf(GREEN"[%ld]"RESET" %d%s\n", time, philo->id, action);
+	pthread_mutex_unlock(philo->mutexes.write_lock);
+}
+
+size_t	get_current_time(void)
+{
+	t_timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		error_message("[gettimeofday ERROR]\n", 1);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+void	ft_usleep(size_t mls)
+{
+	size_t	start;
+
+	start = get_current_time();
+	while (get_current_time() - start < mls)
+		usleep(500);
+}
